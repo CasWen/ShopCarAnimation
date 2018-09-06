db25535214f8a25ad782628165ef6ecc6b89abe6
zhongwenjie <zhong_wenjie@hollywant.com>
2018-09-06 19:02:58 +0800

    fix(master):提交购物车动画效果

---
+++ app/src/main/java/com/zwj/shopcaranimation/model/GoodsModel.java
@@ -0,0 +1,21 @@
+package com.zwj.shopcaranimation.model;
+
+import android.graphics.Bitmap;
+
+/**
+ * <b>Create Date:</b> 2018/9/6<br>
+ * <b>Author:</b> Zhongwenjie <br>
+ * <b>Description:</b>
+ */
+public class GoodsModel {
+    // 商品图
+    private Bitmap mGoodsBitmap;
+
+    public Bitmap getmGoodsBitmap() {
+        return mGoodsBitmap;
+    }
+
+    public void setmGoodsBitmap(Bitmap mGoodsBitmap) {
+        this.mGoodsBitmap = mGoodsBitmap;
+    }
+}
